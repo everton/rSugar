3f7b8f39b4d38d37bbeb2f947cca1734c7492e42
Everton J. Carpes <everton.carpes@gmail.com>
2011-05-19 00:54:21 -0300

    Adicionando nomes para relatorios no exemplo de benchmarks

--- benchmarks.rb
+++ benchmarks.rb
@@ -5,9 +5,9 @@ require 'benchmark'
 
 # puts Benchmark.measure { "a"*1_000_000 }
 
-n = 100_000
+n = 1000_000
 Benchmark.bm do |x|
-  x.report { for i in 1..n; a = "1"; end }
-  x.report { n.times do   ; a = "1"; end }
-  x.report { 1.upto(n) do ; a = "1"; end }
+  x.report('for  ') { for i in 1..n; a = "1"; end }
+  x.report('times') { n.times do   ; a = "1"; end }
+  x.report('upto ') { 1.upto(n) do ; a = "1"; end }
 end
