649728f8971a65f3af48d6f4eba613bbefb914a9
Everton J. Carpes <everton.carpes@gmail.com>
2011-05-24 01:07:29 -0300

    Adicionando exemplo de uso do #set_trace_func

---
+++ trace_funcs.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#-*- coding: utf-8 -*-
+
+class Test
+  def test
+    a = 1
+    b = 2
+  end
+end
+
+set_trace_func proc { |event, file, line, id, binding, classname|
+  printf "%8s %s:%-2d %10s %8s\n", event, file, line, id, classname
+}
+
+t = Test.new
+t.test
