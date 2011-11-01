b769e58ba4b3855e16477a324e6bb4753cdfe6b3
Everton J. Carpes <everton.carpes@gmail.com>
2011-11-01 15:52:53 -0200

    Adicionando mais detalhes no exemplo de observers

--- observers.rb
+++ observers.rb
@@ -13,22 +13,30 @@ class Person
   end
 
   def name=(name)
-    @name = name
-    changed
+    if(@name != name)
+      @name = name
+      changed
+    end
+
     notify_observers(self)
   end
 end
 
 class EmailNotification
-  def initialize(person)
-    person.add_observer(self)
-  end
-
   def update(person)
-    puts "Send email to #{person.name}"
+    puts "Send email to '#{person.name}'"
+  end
+end
+
+class SMSNotification
+  def update(person)
+    puts "Send SMS   to '#{person.name}'"
   end
 end
 
 mulder = Person.new('Fox')
-EmailNotification.new(mulder)
+mulder.add_observer(EmailNotification.new)
+mulder.add_observer(SMSNotification.new)
+
+mulder.name = 'Fox'
 mulder.name = 'Fox Mulder'
