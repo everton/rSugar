e571a2702e1903c87804fafd8bc45ca2d7dbd284
Everton J. Carpes <everton.carpes@gmail.com>
2013-06-11 20:13:22 -0300

    Melhorando exemplo do RubyProf com abertura de arquivo em loop para listar mais system calls

--- profile-sample/profile.rb
+++ profile-sample/profile.rb
@@ -5,7 +5,10 @@ require 'bundler/setup'
 require 'ruby-prof'
 
 result = RubyProf.profile do
-  "a" * 1_000_000
+  100.times do
+    # a = Time.now
+    File.open('delete-me', &:read)
+  end
 end
 
 # RubyProf::GraphPrinter.new(result)
